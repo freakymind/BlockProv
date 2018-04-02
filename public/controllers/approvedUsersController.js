82af5dcd21d8067a3f73a78d9f869d1c30f30c07
insomniac1426 <yashbansal14296@gmail.com>
2018-04-03 00:01:40 +0530

    some changes to approved user UI added modal to UI for add user list

--- public/controllers/approvedUsersController.js
+++ public/controllers/approvedUsersController.js
@@ -2,10 +2,22 @@ angular.module('approvedUsersController', [])
 
 .controller('approvedUsers', ["$http", function($http){
 	var _this = this
+	_this.approvedUsersHeader = "";
+	_this.approvedUsersMessage= "";
+
 	_this.addApprovedUsers = function(users){
 		$http.post('/api/addApprovedUserList', {newUserList : users})
 		.then(function(res){
-			console.log(res);
+			_this.clearout();
+			if (res.data.success) {
+				_this.approvedUsersHeader = "Success"
+				_this.approvedUsersMessage = res.data.message
+				$('#approvedUsersModal').modal({backdrop:"static"});
+			} else {
+				_this.approvedUsersHeader = "Oops"
+				_this.approvedUsersMessage = res.data.message
+				$('#approvedUsersModal').modal({backdrop:"static"});
+			}
 		});
 	}
 
@@ -14,5 +26,9 @@ angular.module('approvedUsersController', [])
 		.then(function(res){
 			console.log(res);
 		});
+	}
+
+	_this.clearout = function() {
+		_this.UserListAdd = "";
 	}
 }]);
