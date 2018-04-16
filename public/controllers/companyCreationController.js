3dec323ae4773a79001c4f2871f3e56634d18696
insomniac1426 <yashbansal14296@gmail.com>
2018-04-16 22:44:46 +0530

    added modal to company creation

--- public/controllers/companyCreationController.js
+++ public/controllers/companyCreationController.js
@@ -2,11 +2,35 @@ angular.module('companyCreationController',[])
 
 .controller('companyCreation',["$http", function($http){
 	var _this = this;
+	_this.modalHead = "";
+	_this.modalBody = "";
+	
+	_this.addCompany = function(valid) {
+		if(valid) {
+			$http.post('/api/addCompany', _this.companyData)
+			.then(function(res){
+				if (res.data.success) {
+					_this.modalHead = "Success";
+					_this.modalBody = res.data.message;
+				} else {
+					_this.modalHead = "Oops";
+					_this.modalBody = res.data.message;
+					
+				}
+				return res;
+			})
+			.then(function(res){
+				$('#companyCreationModal').modal({backdrop:"static"});
+				_this.clearOut();
+			});
+		} else {
+			_this.modalHead = "Failed";
+			_this.modalBody = "All fields are Required";
+			$('#companyCreationModal').modal({backdrop:"static"});
+		}
+	}
 
-	_this.addCompany = function() {
-		$http.post('/api/addCompany', _this.companyData)
-		.then(function(res){
-			console.log(res.data);
-		});
+	_this.clearOut = function(){
+		_this.companyData = {};
 	}
 }]);
